ffc6653074aac1327509e07c1a8bc27ef0f25bee
cancancann <hikmetcanoyke@outlook.com>
2022-10-11 01:49:23 +0300

    header exist

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,12 +1,21 @@
 
 import { Menu,Dropdown,Layout,Avatar} from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 import './AppHeader.css';
+import {setUser} from '../../store/authSlice/AuthSlice'
+import { useNavigate } from 'react-router-dom';
 
 
 const AppHeader = () => {
+    const navigate= useNavigate()
+    const {user}=useSelector((state)=>state.auth)
+    const dispatch = useDispatch()
     const menu = () => {
         return <Menu theme="dark">
-            <Menu.Item key="exit">
+            <Menu.Item key="exit" onClick={()=>{
+                dispatch(setUser(null))
+                navigate('/auth/sign-in')
+            }}>
                 Çıkış Yap
             </Menu.Item>
             
@@ -19,7 +28,7 @@ const AppHeader = () => {
         <Layout.Header className="app-header">
             <Dropdown overlay={menu} placement="bottomLeft" arrow>
                 <Avatar style={{background:"#87d068"}}>
-                    H
+                    {user && user.user.full_name[0]}
                 </Avatar>
             </Dropdown>
         </Layout.Header>
