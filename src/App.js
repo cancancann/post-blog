493510d90dcb03c43932878e492b8d45db929331
cancancann <hikmetcanoyke@outlook.com>
2022-10-12 21:41:58 +0300

    socket bağlantısı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './router/Router';
+import { socket } from './socket/socket';
+import { setSocketID } from './store/socketSlice/SocketSlice';
+import store from './store/store'
 
 function App() {
-  
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      store.dispatch(setSocketID(socket.id))
+    })
+  })
   return (
     <>
       <AppRouter />
